d76e36bfbe36a93021d5dd95e34cb6be99b0c297
p22jiacono <p22jiacono@fairfieldprep.org>
2021-10-04 09:56:27 -0400

    add file 5-1

---
+++ lab_5-1.py
@@ -0,0 +1,21 @@
+#Author: JTI 10/4/21
+
+# Question 1
+#int(a)
+# NameError
+
+# Question 2
+#int('a')
+# ValueErorr
+
+# Question 3
+#'a'+2
+# TypeError
+
+# Question 4
+#import date
+# ModuleNotFoundError
+
+# Question 5
+#print("I'm a happy camper!)
+# SyntaxError
